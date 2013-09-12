371cfaf8d8f4c7a486e24cad75d694ce379afddc
rajba3 <badrinraja@live.com>
2013-09-12 15:13:53 +0530

    Commit Test

--- GitCommit/src/com/example/gitcommit/MainActivity.java
+++ GitCommit/src/com/example/gitcommit/MainActivity.java
@@ -18,8 +18,6 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		System.out.println("Hello");
 		System.out.println("Hello");
-		System.out.println("Hello");
-		System.out.println("Hello");
 		return true;
 	}
 
